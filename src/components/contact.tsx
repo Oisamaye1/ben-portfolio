c80bdb5820a8df416e3cb298ca8c91bea7e6f6a3
Oisamaye1 <ovibenz@gmail.com>
2025-03-03 19:42:24 +0300

    Email

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -3,10 +3,8 @@
 import { motion } from "framer-motion"
 import { Github, Linkedin, Mail } from "lucide-react"
 import Link from "next/link"
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-import { Textarea } from "@/components/ui/textarea"
 import MaxWidthWrapper from "./MaxWidthWrapper"
+import Email from "@/emails"
 
 export default function Contact() {
   return (
@@ -47,14 +45,7 @@ export default function Contact() {
             viewport={{ once: true }}
             className="space-y-4"
           >
-            <form className="space-y-4">
-              <Input placeholder="Name" className="border-[#3E7B27]/60"/>
-              <Input type="email" placeholder="Email" className="border-[#3E7B27]/60"/>
-              <Textarea className="min-h-[100px] border-[#3E7B27]/60" placeholder="Message"  />
-              <Button type="submit" className="w-full bg-[#123524]">
-                Send Message
-              </Button>
-            </form>
+            <Email />
           </motion.div>
         </motion.div>
       </div>
